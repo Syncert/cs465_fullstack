b26087133a46145333d12983e457f9892bb906ac
Nicholas Kreuziger <syncert@gmail.com>
2025-07-16 11:45:14 -0500

    Module 3 , added rooms hbr implementation

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,7 +1,11 @@
-/* GET Homepage */
+var fs = require('fs');
+var roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
 
+console.log(roomsData);
+
+/* GET Rooms */
 const rooms = (req,res) => {
-    res.render('rooms', { title: "Travlr Getaways"});
+    res.render('rooms', { title: "Travlr Getaways", roomsData});
 };
 
 module.exports = {
